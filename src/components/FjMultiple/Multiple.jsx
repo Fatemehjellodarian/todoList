ff6595ddef60df7993b8a1af4981a439a0528b7d
Fatemeh <fatemehjellodarian@gmail.com>
2024-01-31 15:46:00 +0330

    improve

--- src/components/FjMultiple/Multiple.jsx
+++ src/components/FjMultiple/Multiple.jsx
@@ -1,27 +1,67 @@
 
-    const Multiple = ({gender,handleOnselect,handleOnsubmit,})=>{
+const Multiple = ({ gender, handleOnselect, handleOnsubmit }) => {
+  // const projectPhotos = 
+  //   [photo:
+  //     {firstProjectPhoto:<img className="image-1" src="https://colorlib.com/etc/regform/colorlib-regform-26/images/image-1.png" alt=""/>},
+  //     {secondProjectPhoto: <img className="image-1" src="./src/assets/360_F_113995750_dAEGvjqxnsYD6asKjeDWJoVoSqjFvdGO-removebg-preview.png" alt=""></img>}
+  //   }
+  // ];
+   
 
-      
-     const optionsList =[
-      {value:"female" ,text:"female", },
-      {value:"male",text:"male",}
-    ]
- 
-    return(
-     <div>
-        <label className="lable">  Gender</label>
-        <select className="formcontrol"id="select"   value={gender} onChange={handleOnselect} handleOnsubmit={handleOnsubmit}  >
-          {optionsList.map(({value,text})=>(
-            <option value={value}  >{text}</option>
-            
+  const optionsList = [
+    {value:"gender", text:"gender",},
+    { value: "female", text: "female"},
+    { value: "male", text: "male"},
+  ];
+
+  return (
+    <div  >
+       <label className="lable">click here</label>
+      <select
+        className="formcontrol"
+        id="select"
+        value={gender}
+        onChange={handleOnselect}
+        handleOnsubmit={handleOnsubmit}
+       
+      > 
+        {optionsList.map(({ value, text,}) => (
+          <option  value={value}>{text}</option>
          
-          ))}
-          
-        </select></div>
+        ))}
         
+      </select> 
     
-  
-    )
-}
+    </div>
+  );
+};
 export default Multiple;
+
+
+
+
+// import React, { useState } from 'react';
+// import Select from 'react-select';
+
+// const options = [
+//   { value: 'female', text: 'female' },
+//   { value: 'male', text: 'male' },
+
+// ];
+
+// const Multiple = ({ gender,handleOnselect, handleOnsubmit }) =>{
+//   // const [selectedOption, setSelectedOption] = useState(null);
+  
+
+//   return (
  
+//       <Select
+//        handleOnsubmit={handleOnsubmit}
+//         defaultValue={gender}
+//         onChange={handleOnselect}
+//         options={options}
+//       />
+ 
+//   );
+// }
+// export default Multiple;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,46 +1,58 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import InputTodo from "../FjInput/InputTodo";
 import Multiple from "../FjMultiple/Multiple";
 import ButtonTodo from "../FjButton/ButtonTodo";
 import Notes from "./Fjnotes/Notes";
 
-
-
-
 const Id = () => Math.floor(Math.random() * 1000);
-function TodoList() {
+export default function TodoList() {
   const [name, setName] = useState("");
-  const[gender,setGender] = useState("");
+  const [gender, setGender] = useState("");
   const [list, setList] = useState([]);
- 
-  const handleOnchange = ({ target: { value } }) =>setName(value);
-  const handleOnselect= ({target:{value}})=>setGender(value);
-  
-  const newData = { name, id:Id(),gender };
+
+  const handleOnchange = ({ target: { value } }) => setName(value);
+  const handleOnselect = ({ target: { value } }) => setGender(value);
+
+  const newData = { name, id: Id(), gender };
   const handleOnsubmit = () => {
-  setGender("");
+    setGender("");
     setName("");
     setList([...list, newData]);
   };
-  const handleDelete = (id) =>setList((notes) => notes.filter((n) => n.id !== id));
+
+  const handleDelete = (id) =>
+    setList((notes) => notes.filter((n) => n.id !== id));
 
   return (
-    <div className="inner">
-      <h1>wellcome</h1>
-      <InputTodo name={name} handleOnchange={handleOnchange} />
-      <Multiple  gender={gender}  handleOnselect={handleOnselect} handleOnsubmit={handleOnsubmit} />
-      <ButtonTodo handleOnsubmit={handleOnsubmit}  name={name}  />
-      <Notes handleDelete={handleDelete} list={list} />
-    </div>
-  )
+    <>
+      {gender === "female" ? (
+        <img
+          className="image-1"
+          src="https://colorlib.com/etc/regform/colorlib-regform-26/images/image-1.png"
+          alt=""
+        />
+      ) : (
+        <img
+          className="image-1"
+          width="550px"
+          height="550px"
+          src="./src/assets/images/man.png"
+          alt=""
+        ></img>
+      )}
+
+      <div className="inner">
+        <h1>wellcome</h1>
+        <InputTodo name={name} handleOnchange={handleOnchange} />
+        <Multiple
+          gender={gender}
+          handleOnselect={handleOnselect}
+          handleOnsubmit={handleOnsubmit}
+        />
+        <ButtonTodo handleOnsubmit={handleOnsubmit} name={name} />
+        <Notes handleDelete={handleDelete} list={list} />
+      </div>
+    </>
+  );
 }
-
-export default TodoList;
-
-
-
-
-
-
-
