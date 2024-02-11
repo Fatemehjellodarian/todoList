f0a7b466a4a986cd5a9f2e2452943c509930669b
fatemehjellodarian <fatemehjellodarian@gmail.com>
2024-02-11 21:30:11 +0330

    remove [scrollmethodes fn]

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -5,13 +5,18 @@ import Multiple from "../FjMultiple/Multiple";
 import ButtonTodo from "../FjButton/ButtonTodo";
 import Notes from "../Fjnotes/Notes";
 import InputAge from "../DateInput/InputAge";
-import { idGenerator } from "../../helpers/funcs";
+import {
+  idGenerator,
+  scrollStatus,
+  scrollWithId,
+  scrollWithParams,
+  scrollWithRef,
+} from "../../helpers/funcs";
 import styles from "./ProfileCard.module.css";
 
 const canSubmit_V2 = ({ name, gender, age, lastName }) => {
   return name && gender && age && lastName;
 };
-
 
 export default function ProfileCard({ gender, setGender }) {
   const [name, setName] = useState("");
@@ -42,48 +47,18 @@ export default function ProfileCard({ gender, setGender }) {
         id: idGenerator(),
       },
     ]);
-    console.log(list);
   };
 
-  const scrollingMethods = ({list}) => {
-    if (scrollWithParams) {
-      const scrollWithParams = ({ container, top, behavior }) => {
-        container.current.scrollTo({
-          top: top,
-          behavior: behavior,
-        });
-      };
-    }
-
-    if (scrollWithId) {
-      const scrollWithId = ({ behavior }) => {
-        document.getElementById("notes").scrollIntoView({ behavior: behavior });
-      };
-    }
-    if (scrollWithRef) {
-      const scrollWithRef = ({ container, behavior, block }) => {
-        container.current.scrollIntoView({ behavior: behavior, block: block });
-      };
-    }
-    if (scrollStatus) {
-      const scrollStatus = (listLength) => {
-        if (listLength === 0) return 0;
-        if (listLength === 1) return 450;
-        if (listLength > 1) return 450 + (listLength - 1) * 300;
-      };
-    }
-
-    useEffect(() => {
-    scrollWithId({ behavior: "smooth", block: "end" });
-      //  scrollWithRef({container, block:"end" ,behavior:"smooth"});
-      //   scrollStatus(container.current.scrollTo(0,scrollStatus(list.length)));
-      //   scrollWithParams({
-      //     container,
-      //     top: container.current.scrollHeight,
-      //     behavior: "smooth",
-      //   });
-    }, [list]);
-  };
+  useEffect(() => {
+    // scrollWithId({ behavior: "smooth", block: "end" });
+    //  scrollWithRef({container, block:"end" ,behavior:"smooth"});
+    //   scrollStatus(container.current.scrollTo(0,scrollStatus(list.length)));
+    //   scrollWithParams({
+    //     container,
+    //     top: container.current.scrollHeight,
+    //     behavior: "smooth",
+    //   });
+  }, [list]);
 
   const handleDelete = (id) =>
     setList((notes) => notes.filter((n) => n.id !== id));
