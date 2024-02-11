cd7ad58ad9f05d8af2d863d24d8ad199c96cb9d0
fatemehjellodarian <fatemehjellodarian@gmail.com>
2024-02-11 20:51:05 +0330

    remove[element fn]

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -5,17 +5,13 @@ import Multiple from "../FjMultiple/Multiple";
 import ButtonTodo from "../FjButton/ButtonTodo";
 import Notes from "../Fjnotes/Notes";
 import InputAge from "../DateInput/InputAge";
-import { idGenerator, scrollStatus } from "../../helpers/funcs";
+import { idGenerator } from "../../helpers/funcs";
 import styles from "./ProfileCard.module.css";
-
-import { scrollWithParams } from "../../helpers/funcs";
-import { scrollWithId } from "../../helpers/funcs";
-import { scrollWithRef } from "../../helpers/funcs";
 
 const canSubmit_V2 = ({ name, gender, age, lastName }) => {
   return name && gender && age && lastName;
 };
-const element = document.getElementById("notes");
+
 
 export default function ProfileCard({ gender, setGender }) {
   const [name, setName] = useState("");
@@ -24,7 +20,6 @@ export default function ProfileCard({ gender, setGender }) {
   const [age, setAge] = useState("");
   const [list, setList] = useState([]);
   console.log(list);
-
   const handleOnchange = ({ target: { value } }) => setName(value);
   const handleOnchangeLastName = ({ target: { value } }) => setLastName(value);
   const handleOnchangeAge = ({ target: { value } }) => setAge(value);
@@ -50,80 +45,73 @@ export default function ProfileCard({ gender, setGender }) {
     console.log(list);
   };
 
+  const scrollingMethods = ({list}) => {
+    if (scrollWithParams) {
+      const scrollWithParams = ({ container, top, behavior }) => {
+        container.current.scrollTo({
+          top: top,
+          behavior: behavior,
+        });
+      };
+    }
 
-
-  //  container.current.scrollIntoView({ behavior: "smooth", block: "end" });
-
-  
-
- 
-
-  
-  const scrollMethods = () => {
     if (scrollWithId) {
+      const scrollWithId = ({ behavior }) => {
+        document.getElementById("notes").scrollIntoView({ behavior: behavior });
+      };
     }
     if (scrollWithRef) {
-   
+      const scrollWithRef = ({ container, behavior, block }) => {
+        container.current.scrollIntoView({ behavior: behavior, block: block });
+      };
     }
     if (scrollStatus) {
-    
+      const scrollStatus = (listLength) => {
+        if (listLength === 0) return 0;
+        if (listLength === 1) return 450;
+        if (listLength > 1) return 450 + (listLength - 1) * 300;
+      };
     }
-    if (scrollWithParams) {
-  
-    }
+
+    useEffect(() => {
+    scrollWithId({ behavior: "smooth", block: "end" });
+      //  scrollWithRef({container, block:"end" ,behavior:"smooth"});
+      //   scrollStatus(container.current.scrollTo(0,scrollStatus(list.length)));
+      //   scrollWithParams({
+      //     container,
+      //     top: container.current.scrollHeight,
+      //     behavior: "smooth",
+      //   });
+    }, [list]);
   };
-  
-      
-
-
-
-  useEffect(() => {
-    // scrollWithId({ behavior: "smooth" });
-    // scrollWithRef({container, block:"end" ,behavior:"smooth"});
-    //  container.current.scrollTo(0,scrollStatus(list.length));
-   scrollWithParams({
-     container,
-     top: container.current.scrollHeight,
-     behavior: "smooth",
-   });
-  
-    //     //  list.length * 450;
-    //     // console.log({ scrollStatus });
-    //     // console.log(`${%c} ${list.length}`, 'background: #222; color: #bada55');
-  }, [list]);
 
   const handleDelete = (id) =>
     setList((notes) => notes.filter((n) => n.id !== id));
 
+  return (
+    <div className={styles.inner} ref={container}>
+      <h1 className={styles.h1}>welcome</h1>
+      <Input value={name} onChange={handleOnchange} placeholder="NAME" />
+      <Input
+        value={lastName}
+        onChange={handleOnchangeLastName}
+        placeholder="LAST NAME"
+      />
+      <InputAge name={age} handleOnchangeAge={handleOnchangeAge} />
+      <Multiple gender={gender} handleOnselect={handleOnselect} />
+      {canSubmit_V2({ name, gender, age, lastName }) ? (
+        <ButtonTodo
+          handleOnsubmit={handleOnsubmit}
+          name={name}
+          gender={gender}
+          age={age}
+          lastName={lastName}
+        />
+      ) : (
+        <button className={styles.disable}></button>
+      )}
 
-
- return (
-  
- <div className={styles.inner} ref={container} >
- <h1 className={styles.h1}>welcome</h1>
- <Input value={name} onChange={handleOnchange} placeholder="NAME" />
- <Input
- value={lastName}
- onChange={handleOnchangeLastName}
- placeholder="LAST NAME"
- />
- <InputAge name={age} handleOnchangeAge={handleOnchangeAge} />
- <Multiple gender={gender} handleOnselect={handleOnselect} />
- {canSubmit_V2({ name, gender, age, lastName }) ? (
- <ButtonTodo
- handleOnsubmit={handleOnsubmit}
- name={name}
- gender={gender}
- age={age}
- lastName={lastName} 
-
- />
- ) : (
- <button className={styles.disable}></button>
- )}
- {/* <button onClick={scrollHandler}>scroll</button> */}
-
- <Notes handleDelete={handleDelete} list={list}   />
- </div>
- );
-   }
+      <Notes handleDelete={handleDelete} list={list} />
+    </div>
+  );
+}
--- src/helpers/funcs.js
+++ src/helpers/funcs.js
@@ -1,23 +1,22 @@
 export const idGenerator = () => Math.floor(Math.random() * 1000);
 
-export const scrollWithParams = ({ container, top, behavior }) => {
-  container.current.scrollTo({
-    top: top,
-    behavior: behavior,
-  });
-};
-  export const scrollWithId = ({behavior})=>{
-   document.getElementById("notes").scrollIntoView({behavior:behavior});}
+// export const scrollWithParams = ({ container, top, behavior }) => {
+//   container.current.scrollTo({
+//     top: top,
+//     behavior: behavior,
+//   });
+// };
 
-  export const scrollWithRef = ({container,behavior,block})=>{
-      container.current.scrollIntoView({ behavior:behavior, block: block });
-  }
+// export const scrollWithId = ({ behavior }) => {
+//   document.getElementById("notes").scrollIntoView({ behavior: behavior });
+// };
 
-   export const scrollStatus = (listLength,container) => {
-   if (listLength === 0) return 0;
-   if (listLength === 1) return 450;
-   if (listLength > 1) return 450 + (listLength - 1) * 300;
-   container.current.scrollTo(0, scrollStatus(list?.length));
+// export const scrollWithRef = ({ container, behavior, block }) => {
+//   container.current.scrollIntoView({ behavior: behavior, block: block });
+// };
 
-  
-  }
+// export const scrollStatus = (listLength) => {
+//   if (listLength === 0) return 0;
+//   if (listLength === 1) return 450;
+//   if (listLength > 1) return 450 + (listLength - 1) * 300;
+// };
