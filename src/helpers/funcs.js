0f5eb0e270ba6a0c4728b85303a3c3352d412248
fatemehjellodarian <fatemehjellodarian@gmail.com>
2024-02-13 23:07:24 +0330

    feat [funcs]

--- src/helpers/funcs.js
+++ src/helpers/funcs.js
@@ -1,3 +1,56 @@
+// export const idGenerator = () => Math.floor(Math.random() * 1000);
+
+// export const scrollToLastItem = ({
+//   container,
+//   scrollType,
+//   behavior,
+//   listLength,
+// }) => {
+//   const scrollStatusCalc = (listLength) => {
+//     if (listLength === 0) return 0;
+//     if (listLength === 1) return 450;
+//     if (listLength > 1) return 450 + (listLength - 1) * 300;
+//   };
+//   if (
+//     scrollType === "scrollWithRef" ||
+//     scrollType === "scrollWithId" ||
+//     scrollType === "scrollWithParams"
+//   ) {
+//     if (scrollType === "scrollWithRef") {
+//       container.current.scrollIntoView({
+//         behavior,
+//         block: "end",
+//         inline: "end",
+//       });
+//     }
+
+//     if (scrollType === "scrollWithId") {
+//       document.getElementById("notes").scrollIntoView({ behavior });
+//     }
+
+//     if (scrollType === "scrollWithParams") {
+//       container.current.scrollTo({
+//         behavior,
+//         top: scrollStatusCalc(listLength),
+//         left: 0,
+//       });
+//     }
+//   } else {
+//     console.log("تایپ مناسب نمی باشد");
+//   }
+//   if(typeof listLength !=="number"){
+//    console.log(" listLength عدد نیس")
+//   }
+//   if (behavior !== "smooth" && behavior !== "instance" && behavior !== "auto") {
+//     return console.log(" behavior مناسب نیس");
+//   }
+ 
+
+//   return;
+// };
+
+
+
 export const idGenerator = () => Math.floor(Math.random() * 1000);
 
 export const scrollToLastItem = ({
@@ -11,26 +64,42 @@ export const scrollToLastItem = ({
     if (listLength === 1) return 450;
     if (listLength > 1) return 450 + (listLength - 1) * 300;
   };
+  if (
+    scrollType === "scrollWithRef" ||
+    scrollType === "scrollWithId" ||
+    scrollType === "scrollWithParams"
+  ) {
+    if (scrollType === "scrollWithRef") {
+      container.current.scrollIntoView({
+        behavior,
+        block: "end",
+        inline: "end",
+      });
+    }
 
-  if (scrollType === "scrollWithRef" ) {
-    container.current.scrollIntoView({
-      behavior,
-      block: "end",
-      inline: "end",
-    });
-  }
+    if (scrollType === "scrollWithId") {
+      document.getElementById("notes").scrollIntoView({ behavior });
+    }
 
-  if (scrollType === "scrollWithId") {
-    document.getElementById("notes").scrollIntoView({ behavior });
+    if (scrollType === "scrollWithParams") {
+      container.current.scrollTo({
+        behavior,
+        top: scrollStatusCalc(listLength),
+        left: 0,
+      });
+    }
+  } else {
+   
+    throw Error ("type error")
   }
+  if(typeof listLength !=="number"){
+    throw Error ("listLength not a number")
 
-  if (scrollType === "scrollWithParams") {
-    container.current.scrollTo({
-      behavior,
-      top: scrollStatusCalc(listLength),
-      left: 0,
-    });
   }
+  if (behavior !== "smooth" && behavior !== "instance" && behavior !== "auto") {
+     throw Error ("behavior not good")
+  }
+ 
 
   return;
 };
