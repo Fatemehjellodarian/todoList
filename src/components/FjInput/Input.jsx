995dd48692752e317f20f96f88a6139c3efa5098
fatemehjellodarian <fatemehjellodarian@gmail.com>
2024-02-18 13:46:37 +0330

    feat [Input,Multiple]

--- src/components/FjInput/Input.jsx
+++ src/components/FjInput/Input.jsx
@@ -1,13 +1,40 @@
 import style from "./Input.module.css";
-const Input = ({ placeholder, onChange, value,type }) => {
-  return (
-    <input
-      className={style.input}
-      placeholder={placeholder}
-      onChange={onChange}
-      value={value}
-      type={type}
-    />
-  );
+
+const Input = ({ placeholder, onChange, value, type }) => {
+  const optionsList = [
+    { id: 3, value: "gender", text: "gender" },
+    { id: "1", value: "female", text: "female" },
+    { id: "2", value: "male", text: "male" },
+  ];
+
+  if (type === "select") {
+    return (
+      <div className={style.formcontrol}>
+        <label className={style.label}>click here</label>
+        <select
+          className={style.multiple}
+          id="select"
+          value={value}
+          onChange={onChange}
+        >
+          {optionsList.map(({ value, text, id }) => (
+            <option key={id} value={value}>
+              {text}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  } else {
+    return (
+      <input
+        className={style.input}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        type={type}
+      />
+    );
+  }
 };
 export default Input;
